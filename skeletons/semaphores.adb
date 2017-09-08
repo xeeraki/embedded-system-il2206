20abdc33210740d78c301c171361d1ae95b05661
tgeiger12 <31512494+tgeiger12@users.noreply.github.com>
2017-09-08 16:45:39 +0200

    Add files via upload

--- skeletons/semaphores.adb
+++ skeletons/semaphores.adb
@@ -2,9 +2,14 @@
 
 package body Semaphores is
    protected body CountingSemaphore is
-      entry Wait -- To be completed
-
-      entry Signal -- To be completed
+      entry Wait when Count > 0 is
+	begin
+	Count := Count - 1;
+	end Wait;
+      entry Signal when MaxCount < 40 is
+	begin
+	Count := Count + 1;
+	end Signal;
    end CountingSemaphore;
 end Semaphores;
 
