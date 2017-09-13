3953c6250e342f1b330316b7133c19f19c533e42
SindujaPrakash <pk.sinduja@gmail.com>
2017-09-13 15:46:45 +0200

    Update semaphores.adb
    Changed  Maxcont to MaxCount

--- semaphore/semaphores.adb
+++ semaphore/semaphores.adb
@@ -5,10 +5,12 @@ package body Semaphores is
       entry Wait when Count > 0 is
 	     begin
 	     Count := Count - 1;
+	     MaxCount:=MaxCount+1;
 	  end Wait;
       entry Signal is
-	     begin when Maxcont < 40;
+	     begin when MaxCount < 40;
 	     Count := Count + 1;
+	     MaxCount := MaxCount-1;
 	  end Signal;
    end CountingSemaphore;
 end Semaphores;
