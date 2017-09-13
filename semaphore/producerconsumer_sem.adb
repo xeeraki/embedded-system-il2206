7065fead4018648d034da3c6899062c83fe1553f
xeeraki <shafai86@gmail.com>
2017-09-13 17:03:46 +0200

    some changes

--- semaphore/producerconsumer_sem.adb
+++ semaphore/producerconsumer_sem.adb
@@ -14,49 +14,51 @@ procedure ProducerConsumer_sem is
    X : Integer; -- Shared Variable
    N : constant Integer := 40; -- Number of produced and comsumed variables
    
-   S: CountingSemaphore(0,N);
+   S: CountingSemaphore(1,1);
    
    pragma Volatile(X); -- For a volatile object all reads and updates of
                        -- the object as a whole are performed directly
                        -- to memory (Ada Reference Manual, C.6)
 
    -- Random Delays
-   subtype Delay_Interval is Integer range 50..250;
-   package Random_Delay is new Ada.Numerics.Discrete_Random (Delay_Interval);
-   use Random_Delay;
-   G : Generator;
+   --subtype Delay_Interval is Integer range 50..250;
+   --package Random_Delay is new Ada.Numerics.Discrete_Random (Delay_Interval);
+   --use Random_Delay;
+   --G : Generator;
 
    task Producer;
 
    task Consumer;
 
    task body Producer is
-      Next : Time;
+      --Next : Time;
    begin
-      Next := Clock;
+      --Next := Clock;
       for I in 1..N loop
          -- Write to X
          S.Wait;
          X := I;
+         --Put("put.....");
          S.Signal;
          --Next 'Release' in 50..250ms
-         Next := Next + Milliseconds(Random(G));
+         --Next := Next + Milliseconds(Random(G));
          Put_Line(Integer'Image(X));
-         delay until Next;
+         --delay until Next;
       end loop;
    end;
 
    task body Consumer is
-      Next : Time;
+      --Next : Time;
    begin
-      Next := Clock;
+      --Next := Clock;
       for I in 1..N loop
          -- Read from X
          S.Wait;
+         --Put("get....");
          Put_Line(Integer'Image(X));
          S.Signal;
-         Next := Next + Milliseconds(Random(G));
-         delay until Next;
+         --Next := Next + Milliseconds(Random(G));
+         --delay until Next;
       end loop;
    end;
 
@@ -65,4 +67,3 @@ begin -- main task
 
    null;
 end ProducerConsumer_sem;
-
