f5040ccc0589ff3feb14f46223bfb7e0175202b9
SindujaPrakash <pk.sinduja@gmail.com>
2017-09-13 15:59:03 +0200

    Update semaphores.adb
    Changed "Begin When" to " Entry When"

--- semaphore/semaphores.adb
+++ semaphore/semaphores.adb
@@ -2,16 +2,17 @@
 
 package body Semaphores is
    protected body CountingSemaphore is
-      entry Wait when Count > 0 is
-	     begin
-	     Count := Count - 1;
-	     MaxCount:=MaxCount+1;
-	  end Wait;
-      entry Signal is
-	     begin when MaxCount < 40;
-	     Count := Count + 1;
-	     MaxCount := MaxCount-1;
-	  end Signal;
+   entry Wait when Count > 0 is
+	begin
+	Count := Count - 1;
+	MaxCount:= MaxCount+1;
+	
+	end Wait;
+      entry Signal when MaxCount < 40 is
+	begin
+	Count := Count + 1;
+	MaxCount:= MaxCount-1;
+	
+	end Signal;
    end CountingSemaphore;
 end Semaphores;
-
