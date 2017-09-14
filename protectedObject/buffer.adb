7b53e9d7adc40d4c608caab7692c1a6da26481bb
SindujaPrakash <pk.sinduja@gmail.com>
2017-09-14 17:12:06 +0200

    Create buffer.adb
    Protected Objects Done

---
+++ protectedObject/buffer.adb
@@ -0,0 +1,20 @@
+package body buffer is
+
+protected body circularbuffer is
+
+   entry Put(value : in Integer) when Count < N is
+      begin
+         A(In_Ptr) := value;
+         In_Ptr := In_Ptr+1;
+         Count := Count + 1;                   
+      end Put;
+    
+    entry Get(value : out Integer) when Count > 0 is
+      begin
+         value:=A(Out_Ptr);
+         Out_Ptr := Out_Ptr+ 1;  
+         Count := Count - 1;      
+         end Get;
+         
+end circularbuffer;
+end buffer;
