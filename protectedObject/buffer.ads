f7a1c16389d0b96f411fbaaf3ab7b1c97ef4705f
SindujaPrakash <pk.sinduja@gmail.com>
2017-09-14 17:11:17 +0200

    Update buffer.ads
    Protected Objects Done

--- protectedObject/buffer.ads
+++ protectedObject/buffer.ads
@@ -1,15 +1,19 @@
-package Buffer is
+package buffer is
+
    N: constant Integer := 4;
    subtype Item is Integer;
    type Index is mod N;
    type Item_Array is array(Index) of Item;
 
-   protected type CircularBuffer is
-      -- To be completed
+   protected type circularbuffer is
+      
+      entry Put(value : in Integer);
+      entry Get(value : out Integer);
+      
    private
       A: Item_Array;
       In_Ptr, Out_Ptr: Index := 0;
       Count: Integer range 0..N := 0;
-   end CircularBuffer;
-end Buffer;
-
+   end circularbuffer;
+   
+end buffer;
