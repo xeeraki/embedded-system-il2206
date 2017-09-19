0d815f8af08d21d4586d6eae622a40d057ff1b07
xeeraki <shafai86@gmail.com>
2017-09-19 18:40:25 +0200

    final

--- protectedObject/buffer.adb
+++ protectedObject/buffer.adb
@@ -5,15 +5,15 @@ protected body circularbuffer is
    entry Put(value : in Integer) when Count < N is
       begin
          A(In_Ptr) := value;
-         In_Ptr := In_Ptr+1;
-         Count := Count + 1;                   
+         In_Ptr    := In_Ptr+1;
+         Count     := Count + 1;                   
       end Put;
     
-    entry Get(value : out Integer) when Count > 0 is
+   entry Get(value : out Integer) when Count > 0 is
       begin
-         value:=A(Out_Ptr);
+         value   := A(Out_Ptr);
          Out_Ptr := Out_Ptr+ 1;  
-         Count := Count - 1;      
+         Count   := Count - 1;      
          end Get;
          
 end circularbuffer;
