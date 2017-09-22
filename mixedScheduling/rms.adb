3f4d022066db28a46681edc0282e87e835a2a3ea
SindujaPrakash <pk.sinduja@gmail.com>
2017-09-22 17:20:13 +0200

    Mixed Scheduling Implemented

--- mixedScheduling/rms.adb
+++ mixedScheduling/rms.adb
@@ -1,14 +1,34 @@
-pragma Task_Dispatching_Policy(FIFO_Within_Priorities);
+pragma Priority_Specific_Dispatching(
+FIFO_Within_Priorities, 20, 30);
+pragma Priority_Specific_Dispatching(
+Round_Robin_Within_Priorities, 1, 10);
+
 
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Real_Time; use Ada.Real_Time;
 
-procedure rms is
+procedure mixedscheduling is
 
    package Duration_IO is new Ada.Text_IO.Fixed_IO(Duration);
    package Int_IO is new Ada.Text_IO.Integer_IO(Integer);
-   
+    
    Start : Time;
+   StartPgm : Time;
+   Dummy : Integer;
+   Index : Integer := 20;
+    
+   function F(N : Integer) return Integer;
+
+   function F(N : Integer) return Integer is
+      X : Integer := 0;
+   begin
+      for Index in 1..N loop
+         for I in 1..5000000 loop
+            X := I;
+         end loop;
+      end loop;
+      return X;
+   end F;
    
    task type T(Id: Integer; Period : Integer) is
       pragma Priority(Id);
@@ -16,17 +36,20 @@ procedure rms is
    
    task body T is
       Next : Time;
-      X : Integer;
+ --     X : Integer;
    begin
-      Next := Start;
+      Next := StartPgm;
       loop
          Next := Next + Milliseconds(Period);
          -- Some dummy function
-         X := 0;
-         for Index in 1..5000000 loop
-            X := X + Index;
-         end loop;
+         Start := Clock;
+         Dummy := F(Index);
+--        Int_IO.Put(Index, 3);
+         Put("Execution Time");
+         Put(" : ");
          Duration_IO.Put(To_Duration(Clock - Start), 3, 3);
+         Put_Line("s");
+         Duration_IO.Put(To_Duration(Clock - StartPgm), 3, 3);
          Put(" : ");
          Int_IO.Put(Id, 2);
          Put_Line("");
@@ -35,35 +58,17 @@ procedure rms is
    end T;
 
    -- Example Task
-   Task_P1 : T(1,3);
-   Task_P2 : T(2,4);
-   Task_P3 : T(3,6);
-   
-   --function F(N : Integer) return Integer;
-
-   --function F(N : Integer) return Integer is
-   --   X : Integer := 0;
-   --begin
-   --   for Index in 1..N loop
-     --    for I in 1..5000000 loop
-     --       X := I;
-     --    end loop;
-    --  end loop;
-    --  return X;
-  -- end F;
-  -- Dummy : Integer;
+   Task_P1 : T(30, 3000);
+   Task_P2 : T(25, 4000);
+   Task_P3 : T(15, 6000);
+   Task_P4 : T(2,9000);
+   Task_P5 : T(4,9000);
+   Task_P6 : T(6,9000);
+      
 begin
-  -- Put_Line("Measurement of Execution Times");
-   --Put_Line("");
-   --for Index in 1..50 loop
-      --Start := Clock;
-      --Dummy := F(Index);
-      --Int_IO.Put(Index, 3);
-      --Put(" : ");
-     -- Duration_IO.Put(To_Duration(Clock - Start), 3, 3);
-      --Put_Line("s");
-   --end loop;
-   
-   Start := Clock;
-   
-end rms;
+
+  -- for Index in 1..50 loop
+      StartPgm := Clock;
+
+  -- end loop;
+end mixedscheduling;
