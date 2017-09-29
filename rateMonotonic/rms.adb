343ed25330ac53d13f00ce8014f08a49066283ab
SindujaPrakash <pk.sinduja@gmail.com>
2017-09-29 09:51:44 +0200

    Updated Rate Monotonic with comments

--- rateMonotonic/rms.adb
+++ rateMonotonic/rms.adb
@@ -7,8 +7,24 @@ procedure rms is
 
    package Duration_IO is new Ada.Text_IO.Fixed_IO(Duration);
    package Int_IO is new Ada.Text_IO.Integer_IO(Integer);
-   
+    
    Start : Time;
+   StartPgm : Time;
+   Dummy : Integer;
+   Index : Integer := 20;  -- Index for which the execution time is 1 sec
+    
+   function F(N : Integer) return Integer;
+
+   function F(N : Integer) return Integer is -- Dummy Function
+      X : Integer := 0;
+   begin
+      for Index in 1..N loop
+         for I in 1..5000000 loop
+            X := I;
+         end loop;
+      end loop;
+      return X;
+   end F;
    
    task type T(Id: Integer; Period : Integer) is
       pragma Priority(Id);
@@ -16,17 +32,20 @@ procedure rms is
    
    task body T is
       Next : Time;
-      X : Integer;
+
    begin
-      Next := Start;
+      Next := StartPgm;
       loop
          Next := Next + Milliseconds(Period);
-         -- Some dummy function
-         X := 0;
-         for Index in 1..5000000 loop
-            X := X + Index;
-         end loop;
-         Duration_IO.Put(To_Duration(Clock - Start), 3, 3);
+       
+         Start := Clock;
+         Dummy := F(Index); -- Call the dummy functiom
+
+         Put("Execution Time");
+         Put(" : ");
+         Duration_IO.Put(To_Duration(Clock - Start), 3, 3); -- execution time of task
+         Put_Line("s");
+         Duration_IO.Put(To_Duration(Clock - StartPgm), 3, 3);-- execution time of program
          Put(" : ");
          Int_IO.Put(Id, 2);
          Put_Line("");
@@ -35,35 +54,10 @@ procedure rms is
    end T;
 
    -- Example Task
-   Task_P1 : T(1,3);
-   Task_P2 : T(2,4);
-   Task_P3 : T(3,6);
+   Task_P10 : T(20, 3000); -- Task with priority 20 and period 3000, execution time 1 sec
+   Task_P12 : T(15, 4000); -- Task with priority 15 and period 4000, execution time 1 sec  
+   Task_P14 : T(10, 6000); -- Task with priority 10 and period 6000, execution time 1 sec
    
-   --function F(N : Integer) return Integer;
-
-   --function F(N : Integer) return Integer is
-   --   X : Integer := 0;
-   --begin
-   --   for Index in 1..N loop
-     --    for I in 1..5000000 loop
-     --       X := I;
-     --    end loop;
-    --  end loop;
-    --  return X;
-  -- end F;
-  -- Dummy : Integer;
 begin
-  -- Put_Line("Measurement of Execution Times");
-   --Put_Line("");
-   --for Index in 1..50 loop
-      --Start := Clock;
-      --Dummy := F(Index);
-      --Int_IO.Put(Index, 3);
-      --Put(" : ");
-     -- Duration_IO.Put(To_Duration(Clock - Start), 3, 3);
-      --Put_Line("s");
-   --end loop;
-   
-   Start := Clock;
-   
+      StartPgm := Clock; -- Start of program
 end rms;
