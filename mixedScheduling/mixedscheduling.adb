394e5b065a357caef54157fcc1c2703e7caab7e5
SindujaPrakash <pk.sinduja@gmail.com>
2017-09-29 09:55:03 +0200

    Updated Mixed Scheduling with comments

--- mixedScheduling/mixedscheduling.adb
+++ mixedScheduling/mixedscheduling.adb
@@ -1,7 +1,7 @@
 pragma Priority_Specific_Dispatching(
-FIFO_Within_Priorities, 20, 30);
+FIFO_Within_Priorities, 20, 30);      -- FIFO has highest priority
 pragma Priority_Specific_Dispatching(
-Round_Robin_Within_Priorities, 1, 10);
+Round_Robin_Within_Priorities, 1, 10); -- Round Robin has lowest priority
 
 
 with Ada.Text_IO; use Ada.Text_IO;
@@ -15,11 +15,11 @@ procedure mixedscheduling is
    Start : Time;
    StartPgm : Time;
    Dummy : Integer;
-   Index : Integer := 20;
+   Index : Integer := 20;  -- Number for which the execution time is 1 sec in Function F
     
    function F(N : Integer) return Integer;
 
-   function F(N : Integer) return Integer is
+   function F(N : Integer) return Integer is -- Dummy function with execution time 1 sec
       X : Integer := 0;
    begin
       for Index in 1..N loop
@@ -36,39 +36,34 @@ procedure mixedscheduling is
    
    task body T is
       Next : Time;
- --     X : Integer;
    begin
       Next := StartPgm;
       loop
          Next := Next + Milliseconds(Period);
-         -- Some dummy function
          Start := Clock;
          Dummy := F(Index);
---        Int_IO.Put(Index, 3);
          Put("Execution Time");
          Put(" : ");
-         Duration_IO.Put(To_Duration(Clock - Start), 3, 3);
+         Duration_IO.Put(To_Duration(Clock - Start), 3, 3); -- Execution Time of that particular task
          Put_Line("s");
-         Duration_IO.Put(To_Duration(Clock - StartPgm), 3, 3);
+         Duration_IO.Put(To_Duration(Clock - StartPgm), 3, 3); -- Overall time elapsed from start of program
          Put(" : ");
          Int_IO.Put(Id, 2);
          Put_Line("");
-         delay until Next;
+         delay until Next; -- Delay until next Period
       end loop;
    end T;
 
-   -- Example Task
-   Task_P1 : T(30, 3000);
+   -- Task with FIFO_Within_Priority Scheduling (High Priority)
+   Task_P1 : T(30, 3000); 
    Task_P2 : T(25, 4000);
    Task_P3 : T(15, 6000);
+   
+   -- Task with Round Robin Scheduling (Low priority)
    Task_P4 : T(2,9000);
    Task_P5 : T(4,9000);
    Task_P6 : T(6,9000);
       
 begin
-
-  -- for Index in 1..50 loop
-      StartPgm := Clock;
-
-  -- end loop;
+      StartPgm := Clock; -- Start of Program
 end mixedscheduling;
