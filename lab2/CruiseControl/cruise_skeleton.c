c4a85dd852cdeb24b4864cb94de5e2bf9acfb614
xeeraki <shafai86@gmail.com>
2017-10-03 18:33:21 +0200

    Switch states defined

--- lab2/CruiseControl/cruise_skeleton.c
+++ lab2/CruiseControl/cruise_skeleton.c
@@ -10,29 +10,35 @@
     #include <time.h>
     #include <sys/alt_timestamp.h>
     #include <sys/alt_cache.h>
-    
-    
-    
+
+
+
     #define N 8192
-    
+
     #define M 32
-    
+
     #define DEBUG 1
-    
+
     #define HW_TIMER_PERIOD 100 /* 100ms */
-    
+
     /* Button Patterns */
-    
+
     #define GAS_PEDAL_FLAG      0x08
     #define BRAKE_PEDAL_FLAG    0x04
     #define CRUISE_CONTROL_FLAG 0x02
     /* Switch Patterns */
-    
+
     #define TOP_GEAR_FLAG       0x00000002
     #define ENGINE_FLAG         0x00000001
-    
+    #define SWITCH_4            0x00000010
+    #define SWITCH_5            0x00000020
+    #define SWITCH_6            0x00000040
+    #define SWITCH_7            0x00000080
+    #define SWITCH_8            0x00000100
+    #define SWITCH_9            0x00000200
+
     /* LED Patterns */
-    
+
     #define LED_RED_0  0x00000001 // Engine
     #define LED_RED_1  0x00000002 // Top Gear
     #define LED_RED_12 0x00001000 //position = [0,400]
@@ -41,49 +47,61 @@
     #define LED_RED_15 0x00008000
     #define LED_RED_16 0x00010000 //position = [1600,2000]
     #define LED_RED_17 0x00020000 //position = [2000m, 2400m]
-    
+
+
+
     #define LED_GREEN_0 0x0001 // Cruise Control activated
     #define LED_GREEN_2 0x0002 // Cruise Control Button
     #define LED_GREEN_3 0x0004 // Cruise Control Button
     #define LED_GREEN_4 0x0010 // Brake Pedal
     #define LED_GREEN_6 0x0040 // Gas Pedal
-    
+
     #define TASK_STACKSIZE 2048
-    
+
     OS_STK StartTask_Stack[TASK_STACKSIZE];
     OS_STK ControlTask_Stack[TASK_STACKSIZE];
     OS_STK VehicleTask_Stack[TASK_STACKSIZE];
     OS_STK WatchdogTask_Stack[TASK_STACKSIZE];
     OS_STK DetectionTask_Stack[TASK_STACKSIZE];
+    OS_STK ExtraLoadTask_Stack[TASK_STACKSIZE];
     // Task Priorities
-    
+
     #define STARTTASK_PRIO      5
     #define VEHICLETASK_PRIO    10
     #define CONTROLTASK_PRIO    12
-    #define DETECTIONTASK_PRIO  13
-    #define WATCHDOGTASK_PRIO   14
-    
-    
+    #define DETECTIONTASK_PRIO  14
+    #define WATCHDOGTASK_PRIO   1
+    #define EXTRALOAD_PRIO      13
+
+
     // Task Periods
-    
+
     #define CONTROL_PERIOD  300
     #define VEHICLE_PERIOD  300
-    
+
     /*
      * Definition of Kernel Objects
      */
-    
+
     // Mailboxes
     OS_EVENT *Mbox_Throttle;
     OS_EVENT *Mbox_Velocity;
     OS_EVENT *Mbox_Writeok;
-    
+
     // Semaphores
-    OS_EVENT *aSemaphore;
-    OS_EVENT *bSemaphore;
-    
+    OS_EVENT *Semaphore1;
+    OS_EVENT *Semaphor2;
+    OS_EVENT *Semaphor3;
+    OS_EVENT *Semaphor4;
+    OS_EVENT *Semaphor5;
+
     // SW-Timer
     OS_TMR *MyTmr1;
+    OS_TMR *MyTmr2;
+    OS_TMR *MyTmr3;
+    OS_TMR *MyTmr4;
+    OS_TMR *MyTmr5;
+
     alt_u32 ticks;
     alt_u32 time_1;
     alt_u32 time_2;
@@ -92,13 +110,16 @@
      * Types
      */
     enum active {on, off};
-    
+
     enum active gas_pedal = off;
     enum active brake_pedal = off;
     enum active top_gear = off;
     enum active engine = off;
     enum active cruise_control = off;
-    
+    enum active status = on;
+
+
+
     /*
      * Global variables
      */
@@ -114,36 +135,36 @@
         // Post to the semaphore to signal that it's time to run the task.
         OSSemPost(aSemaphore); // Releasing the key
         OSSemPost(aSemaphore);
-    
-    
+
+
     }
-    
+
     int buttons_pressed(void)
     {
         return ~IORD_ALTERA_AVALON_PIO_DATA(DE2_PIO_KEYS4_BASE);
     }
-    
+
     int switches_pressed(void)
     {
         return IORD_ALTERA_AVALON_PIO_DATA(DE2_PIO_TOGGLES18_BASE);
     }
-    
+
     /*
      * ISR for HW Timer
      */
     alt_u32 alarm_handler(void* context)
     {
         OSTmrSignal(); /* Signals a 'tick' to the SW timers */
-    
+
         return delay;
     }
-   /* 
+   /*
 
     /*void shownumberonleds(int x)
     {
     IOWR_ALTERA_AVALON_PIO_DATA( LED_PIO_BASE , x ) ;
     }*/
-    
+
     static int b2sLUT[] = {0x40, //0
                            0x79, //1
                            0x24, //2
@@ -156,14 +177,14 @@
                            0x18, //9
                            0x3F, //-
                           };
-    
+
     /*
      * convert int to seven segment display format
      */
     int int2seven(int inval) {
         return b2sLUT[inval];
     }
-    
+
     /*
      * output current velocity on the seven segement display
      */
@@ -173,50 +194,50 @@
         INT8U out_high = 0;
         INT8U out_low = 0;
         INT8U out_sign = 0;
-    
+
         if (velocity < 0) {
             out_sign = int2seven(10);
             tmp *= -1;
         } else {
             out_sign = int2seven(0);
         }
-    
+
         out_high = int2seven(tmp / 10);
         out_low = int2seven(tmp - (tmp/10) * 10);
-    
+
         out = int2seven(0) << 21 |
               out_sign << 14 |
               out_high << 7  |
               out_low;
         IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_HEX_LOW28_BASE,out);
     }
-    
-    
+
+
     /*
      * output current position on the seven segement display
      */
     void show_position_on_sevenseg(INT8S position) {
-    
-    
+
+
     }
-    
+
     void show_target_velocity(INT8S velocity) {
         int tmp = velocity;
         int out;
         INT8U out_high = 0;
         INT8U out_low = 0;
         INT8U out_sign = 0;
-    
+
         if (velocity < 0) {
             out_sign = int2seven(10);
             tmp *= -1;
         } else {
             out_sign = int2seven(0);
         }
-    
+
         out_high = int2seven(tmp / 10);
         out_low = int2seven(tmp - (tmp/10) * 10);
-    
+
         out = int2seven(0) << 21 |
               out_sign << 14 |
               out_high << 7  |
@@ -225,11 +246,11 @@
             IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_HEX_HIGH28_BASE,out);
             IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_GREENLED9_BASE, LED_GREEN_0);
         } else {
-    
+
             IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_HEX_HIGH28_BASE,0x00);
         }
     }
-    
+
     /*
      * indicates the position of the vehicle on the track with the four leftmost red LEDs
      * LEDR17: [0m, 400m)
@@ -241,7 +262,7 @@
      */
     void show_position(INT16U position)
     {
-     if (position < 4000) 
+     if (position < 4000)
         if (engine==on)
             if(top_gear==on)
             // Turn ON only those LED below remain unchanged the other LEDS
@@ -253,7 +274,7 @@
                 IOWR_ALTERA_AVALON_PIO_DATA (DE2_PIO_REDLED18_BASE,LED_RED_1|LED_RED_17);
             else
                 IOWR_ALTERA_AVALON_PIO_DATA (DE2_PIO_REDLED18_BASE,LED_RED_17);
-                
+
     else
       if (position < 8000)
           if (engine==on)
@@ -266,8 +287,8 @@
                   IOWR_ALTERA_AVALON_PIO_DATA (DE2_PIO_REDLED18_BASE,LED_RED_1|LED_RED_16);
               else
                   IOWR_ALTERA_AVALON_PIO_DATA (DE2_PIO_REDLED18_BASE,LED_RED_16);
-                  
-      else 
+
+      else
         if (position < 12000)
             if (engine==on)
                 if(top_gear==on)
@@ -279,7 +300,7 @@
                     IOWR_ALTERA_AVALON_PIO_DATA (DE2_PIO_REDLED18_BASE,LED_RED_1|LED_RED_15);
                 else
                     IOWR_ALTERA_AVALON_PIO_DATA (DE2_PIO_REDLED18_BASE,LED_RED_15);
-                    
+
         else
           if (position < 16000)
                 if (engine==on)
@@ -292,7 +313,7 @@
                         IOWR_ALTERA_AVALON_PIO_DATA (DE2_PIO_REDLED18_BASE,LED_RED_1|LED_RED_14);
                     else
                         IOWR_ALTERA_AVALON_PIO_DATA (DE2_PIO_REDLED18_BASE,LED_RED_14);
-                        
+
           else
             if (position < 20000)
                 if (engine==on)
@@ -317,9 +338,9 @@
                     else
                         IOWR_ALTERA_AVALON_PIO_DATA (DE2_PIO_REDLED18_BASE,LED_RED_12);
 
-        
+
     }
-    
+
     /*
      * The function 'adjust_position()' adjusts the position depending on the
      * acceleration and velocity.
@@ -329,17 +350,17 @@
     {
         INT16S new_position = position + velocity * time_interval / 1000
                               + acceleration / 2  * (time_interval / 1000) * (time_interval / 1000);
-    
+
         if (new_position > 24000) {
             new_position -= 24000;
         } else if (new_position < 0) {
             new_position += 24000;
         }
-    
+
         //show_position(new_position);
         return new_position;
     }
-    
+
     /*
      * The function 'adjust_velocity()' adjusts the velocity depending on the
      * acceleration.
@@ -349,7 +370,7 @@
     {
         INT16S new_velocity;
         INT8U brake_retardation = 200;
-    
+
         if (brake_pedal == off)
             new_velocity = velocity  + (float) (acceleration * time_interval) / 1000.0;
         else {
@@ -358,10 +379,10 @@
             else
                 new_velocity = velocity - brake_retardation * time_interval / 1000;
         }
-    
+
         return new_velocity;
     }
-    
+
     int cruise_velocity =0;
     int cruise_control_increase_velocity = 0;
     int cruise_control_decrease_velocity = 0;
@@ -383,13 +404,13 @@
                 cruise_control_decrease_velocity = 1;
                 IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_GREENLED9_BASE, LED_GREEN_0);
             } else {
-    
+
                 IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_GREENLED9_BASE, 0x0000);
             }
         }
         printf("buttons_pressed: %d\n",buttons_pressed() );
     }
-    
+
     void Button2IO(INT16S current_velocity)
     {
         if (buttons_pressed() == -12) {
@@ -435,7 +456,7 @@
     void initArray(int x[], int n)
     {
         int i;
-    
+
         for (i = 0; i < n; i++)
             x[i] = i;
     }
@@ -462,7 +483,7 @@
         int size = M;
         int i, j;
         timer_overhead = 0;
-    
+
         if (switches_pressed()==1) {
             engine = on;
             top_gear=off;
@@ -480,7 +501,7 @@
             show_position(position);
         }
         else if (switches_pressed()==2) {
-    
+
         }
         else if ((switches_pressed()-19)/16 >= 0) {
             int number = (switches_pressed()-19)/16+1;
@@ -496,20 +517,20 @@
             initArray(w, 600);
             initArray(x, 13);
             start_measurement();
-    
+
             j=tmp+number*4;
             for (i = 0; i < j; i++)
                 w[i]++;
             stop_measurement();
             printf("%5.2f us", (float) microseconds(ticks - timer_overhead));
             printf("(%d ticks)\n", (int) (ticks - timer_overhead));
-    
+
         }
         printf("switches_pressed: %d\n", switches_pressed());
         printf("switches_pressed: %d\n", (switches_pressed()-19)/16);
-    
+
     }
-    
+
     void pollkey()
     {
         int btn_reg;
@@ -538,15 +559,15 @@
             }
         }
     }
-    
-    
+
+
     int globalPosition = 0;
-    
+
     void setGlobalPosition(int position) {
-    
+
         globalPosition = position;
     }
-    
+
     int getGlobalPosition() {
         return globalPosition;
     }
@@ -563,7 +584,7 @@
         INT16U position = 0; /* Value between 0 and 20000 (0.0 m and 2000.0 m)  */
         INT16S velocity = 0; /* Value between -200 and 700 (-20.0 m/s amd 70.0 m/s) */
         INT16S wind_factor;   /* Value between -10 and 20 (2.0 m/s^2 and -1.0 m/s^2) */
-    
+
         printf("Vehicle task created!\n");
         while (1)
         {
@@ -576,13 +597,13 @@
             msg = OSMboxPend(Mbox_Throttle, 1, &err);
             if (err == OS_NO_ERR)
                 throttle = (INT8U*) msg;
-    
+
             /* Retardation : Factor of Terrain and Wind Resistance */
             if (velocity > 0)
                 wind_factor = velocity * velocity / 10000 + 1;
             else
                 wind_factor = (-1) * velocity * velocity / 10000 + 1;
-    
+
             if (position < 4000)
                 retardation = wind_factor; // even ground
             else if (position < 8000)
@@ -595,7 +616,7 @@
                 retardation = wind_factor - 10; //traveling downhill
             else
                 retardation = wind_factor - 5 ; // traveling steep downhill
-    
+
             acceleration = *throttle / 2 - retardation;
             position = adjust_position(position, velocity, acceleration, 300);
             setGlobalPosition(position);
@@ -609,12 +630,12 @@
             Button2IO( (velocity/10) );
         }
     }
-    
+
     /*
      * The task 'ControlTask' is the main task of the application. It reacts
      * on sensors and generates responses.
      */
-    
+
     void ControlTask(void* pdata)
     {
         INT8U err;
@@ -624,10 +645,10 @@
         int btn_reg;
         INT16S* current_output;
         printf("Control Task created!\n");
-    
+
         while (1)
         {
-    
+
             OSSemPend(aSemaphore, 1, &err); // Trying to access the key
             msg = OSMboxPend(Mbox_Velocity, 0, &err);
             current_velocity = (INT16S*) msg;
@@ -643,20 +664,20 @@
                 if (*current_velocity <= cruise_velocity) {
                     throttle = throttle + increment - 16;
                 }
-    
-    
+
+
                 cruise_control_increase_velocity = 0;
             }
             else if (btn_reg == 11) {
                 if (throttle > 0) {
                     --throttle;
                 }
-    
+
             } else if (cruise_control_decrease_velocity == 1 && throttle >= increment && * current_velocity >= cruise_velocity) {
                 //printf("decrease_velocity \n");
-            
+
                     throttle = throttle -increment;
-    
+
                 cruise_control_decrease_velocity = 0;
             }
             if (btn_reg== 13) {
@@ -685,7 +706,7 @@
                 stop_measurement();
                 timer_overhead = timer_overhead + time_2 - time_1;
             }
-    
+
             start_measurement();
             OSSemPend(bSemaphore, 1, &err); // Trying to access the key
             msg = OSMboxPend(Mbox_Writeok, 0, &err);
@@ -694,7 +715,7 @@
             if ( * current_output != 17 ) {
                 printf("System overload:%d\n", * current_output);
             }
-    
+
             stop_measurement();
             float measure = (float) microseconds(ticks - timer_overhead);
             if (measure > 7600 && measure < 10000) {
@@ -702,8 +723,8 @@
             }
             printf("Watchdog %5.2f us", (float) microseconds(ticks - timer_overhead));
             printf("(%d ticks)\n", (int) (ticks - timer_overhead));
-    
-    
+
+
             current_output = 0;
         }
     }
@@ -717,14 +738,14 @@
             OSSemPend(bSemaphore, 0, &err); // Trying to access the key
             err = OSMboxPost(Mbox_Writeok, (void *) &next);
             /* Check “err” */
-    
+
         }
     }
     /*
      * The task 'StartTask' creates all other tasks kernel objects and
      * deletes itself afterwards.
      */
-    
+
     void StartTask(void* pdata)
     {
         INT8U err;
@@ -732,7 +753,7 @@
         IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_REDLED18_BASE,0X00000);
         IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_HEX_HIGH28_BASE,0X00000);
         static alt_alarm alarm;     /* Is needed for timer ISR function */
-    
+
         /* Base resolution for SW timer : HW_TIMER_PERIOD ms */
         delay = alt_ticks_per_second() * HW_TIMER_PERIOD / 1000;
         printf("delay in ticks %d\n", delay);
@@ -748,11 +769,11 @@
         {
             printf("No system clock available!n");
         }
-    
+
         /*
          * Create and start Software Timer
          */
-    
+
         MyTmr1 = OSTmrCreate(0,
                               (CONTROL_PERIOD/100),
                               OS_TMR_OPT_PERIODIC,
@@ -764,7 +785,7 @@
             /* Timer was created */
             printf("Soft timer was created \n");
         }
-    
+
         BOOLEAN status = OSTmrStart(MyTmr1,
                                     &err);
         if (status > 0 && err == OS_ERR_NONE) {
@@ -774,22 +795,22 @@
         /*
          * Creation of Kernel Objects
          */
-    
+
         // Mailboxes
         Mbox_Throttle = OSMboxCreate((void*) 0); /* Empty Mailbox - Throttle */
         Mbox_Velocity = OSMboxCreate((void*) 0); /* Empty Mailbox - Velocity */
         Mbox_Writeok = OSMboxCreate((void*) 0); /* Empty Mailbox - Write ok */
-    
+
         /*
          * Create statistics task
          */
-    
+
         OSStatInit();
-    
+
         /*
          * Creating Tasks in the system
          */
-    
+
         err = OSTaskCreateExt(
                   ControlTask, // Pointer to task code
                   NULL,        // Pointer to argument that is
@@ -802,7 +823,7 @@
                   TASK_STACKSIZE,
                   (void *) 0,
                   OS_TASK_OPT_STK_CHK);
-    
+
         err = OSTaskCreateExt(
                   VehicleTask, // Pointer to task code
                   NULL,        // Pointer to argument that is
@@ -815,7 +836,7 @@
                   TASK_STACKSIZE,
                   (void *) 0,
                   OS_TASK_OPT_STK_CHK);
-    
+
         err = OSTaskCreateExt(
                   WatchDogTask, // Pointer to task code
                   NULL,        // Pointer to argument that is
@@ -828,7 +849,7 @@
                   TASK_STACKSIZE,
                   (void *) 0,
                   OS_TASK_OPT_STK_CHK);
-    
+
         err = OSTaskCreateExt(
                   DetectionTask, // Pointer to task code
                   NULL,        // Pointer to argument that is
@@ -841,21 +862,21 @@
                   TASK_STACKSIZE,
                   (void *) 0,
                   OS_TASK_OPT_STK_CHK);
-    
-    
-    
+
+
+
         printf("All Tasks and Kernel Objects generated!\n");
         /* Task deletes itself */
-    
+
         OSTaskDel(OS_PRIO_SELF);
     }
-    
+
     int main(void) {
-    
+
         printf("Cruise Control 20141010\n");
         aSemaphore = OSSemCreate(1); // binary semaphore (1 key)
         bSemaphore = OSSemCreate(1); // binary semaphore (1 key)
-    
+
         OSTaskCreateExt(
             StartTask, // Pointer to task code
             NULL,      // Pointer to argument that is
@@ -868,11 +889,8 @@
             TASK_STACKSIZE,
             (void *) 0,
             OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
-    
+
         OSStart();
-    
+
         return 0;
     }
-
-
-
