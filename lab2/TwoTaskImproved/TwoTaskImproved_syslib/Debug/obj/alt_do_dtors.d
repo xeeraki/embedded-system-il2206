ececcd2876a0fc5730a4c151d98523ee8469f050
xeeraki <shafai86@gmail.com>
2017-10-06 12:43:25 +0200

    TwoTaskImproved

--- lab2/TwoTaskImproved/TwoTaskImproved_syslib/Debug/obj/alt_do_dtors.d
+++
@@ -1,2 +0,0 @@
-obj/alt_do_dtors.o:  \
- /opt/altera/10.0/ip/altera/nios2_ip/altera_nios2/HAL/src/alt_do_dtors.c
--- lab2/TwoTaskImproved/TwoTaskImproved_syslib/Debug/obj/alt_log_macro.d
+++
@@ -1,2 +0,0 @@
-obj/alt_log_macro.o:  \
- /opt/altera/10.0/ip/altera/nios2_ip/altera_nios2/HAL/src/alt_log_macro.S
